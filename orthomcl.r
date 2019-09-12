8be41159f57249d5929177a93bb50ab01a3b7ef3
Peter Heger <54813618+prheger@users.noreply.github.com>
2019-09-12 17:15:17 +0200

    Update orthomcl.r

--- orthomcl.r
+++ orthomcl.r
@@ -1,3 +1,27 @@
+#    orthomcl.r
+#    the script performs those steps of the original OrthoMCL pipeline (Li et 
+#    al., 2003) which use a MySQL database, within R
+#
+#    Copyright (C) 2019  Wen Zheng, Peter Heger
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Contact: Peter Heger, University of Cologne, Institute for Genetics, 
+#    Zuelpicher Strasse 47a, 50674 Koeln, Germany
+#    Email: peter.heger@uni-koeln.de, spacepeter@gmx.de
+
+
 #!/usr/bin/Rscript
 
 # Author: Wen Zheng
