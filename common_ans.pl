e57e55e6900a0485ce58f3bb2e30e7c81f68ddb8
Peter Heger <54813618+prheger@users.noreply.github.com>
2019-09-12 17:11:35 +0200

    Update common_ans.pl

--- common_ans.pl
+++ common_ans.pl
@@ -1,3 +1,29 @@
+#    common_ans.pl
+#    it computes the last common ancestor of orthogroups obtained in the 
+#    OrthoMCL pipeline along with orthogroup composition
+#
+#    Copyright (C) 2019  Wen Zheng, Peter Heger
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Contact: Peter Heger, University of Cologne, Institute for Genetics, 
+#    Zuelpicher Strasse 47a, 50674 Koeln, Germany
+#    Email: peter.heger@uni-koeln.de, spacepeter@gmx.de
+
+
+# Author: Wen Zheng
+
 #!/usr/bin/perl
 
 use Cwd 'abs_path';
